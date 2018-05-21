7162f66368efa064fddb8f8a49c9de18a9f87edf
olive <liurc@outlook.com>
2018-05-21 11:46:37 +0800

    init

---
+++ index.js
@@ -0,0 +1,13 @@
+import alert from "weui.js/src/alert"
+import toast from "weui.js/src/toast"
+import confirm from "weui.js/src/confirm"
+
+export const alert = alert;
+export const toast = toast;
+export const confirm = confirm;
+
+export default {
+    alert,
+    toast,
+    confirm
+}
